2a31d824a38819d4f3de4ee8ab2bb30010882815
takwach5 <channoufitakwa09@gmail.com>
2024-03-22 13:01:55 +0100

    payment

--- backend/Routes/routechat.js
+++ backend/Routes/routechat.js
@@ -1,11 +1,11 @@
-const routerchat=require('express').Router()
+// const routerchat=require('express').Router()
 
-const controllerchat=require('../Controller/controllerchat')
+// const controllerchat=require('../Controller/controllerchat')
 
-routerchat.post('/createchat',controllerchat.createChat)
-routerchat.post("/createmessage", controllerchat.createMessage);
-routerchat.get("/getallmessage/:chatId", controllerchat.getallMessage);
-routerchat.get("/getallchats/:id/", controllerchat.getAllChats);
-routerchat.delete("/deletechat/:chatId", controllerchat.deleteChat);
+// routerchat.post('/createchat',controllerchat.createChat)
+// routerchat.post("/createmessage", controllerchat.createMessage);
+// routerchat.get("/getallmessage/:chatId", controllerchat.getallMessage);
+// routerchat.get("/getallchats/:id/", controllerchat.getAllChats);
+// routerchat.delete("/deletechat/:chatId", controllerchat.deleteChat);
 
-module.exports=routerchat
+// module.exports=routerchat
